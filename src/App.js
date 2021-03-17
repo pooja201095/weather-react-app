4c7247e2fa9d760382b0429afb5f68632f940473
poochava1 <pooja.chavan2@publicissapient.com>
2021-03-17 22:38:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+  baseUrl:"https://api.openweathermap.org/data/2.5/weather"
 }
+export default function App() {
+  let [location,setLocation] = useState('');
+  let [weather, setWeather] = useState({});
 
-export default App;
+  function onInputChange(e) {
+    setLocation(e.target.value);
+  }
+
+  const dateBuilder =(d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`
+  }
+
+  const triggerSearch =(e) =>{
+    if(e.key == "Enter") {
+      fetch(`${api.baseUrl}?q=${location}&units=metric&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
+      .then(res => res.json())
+      .then(res=>{
+        setWeather(res);
+        setLocation('');
+      });
+    }
+  }
+
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search.." value={location} onChange={onInputChange} onKeyPress={triggerSearch}/>
+        </div>
+        {(typeof weather.main != "undefined") ?
+        (<div className="info"><div className="location-box">
+          <div className="location">
+            {weather.name} , {weather.sys.country}
+          </div>
+          <div className="date">
+            {dateBuilder(new Date())}
+          </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            {weather.main.temp}°C
+          </div>
+          <div className="weather">
+            {weather.weather[0].main}
+          </div>
+        </div> </div>): ''}
+      </main>
+    </div>
+  ); 
+
+}
